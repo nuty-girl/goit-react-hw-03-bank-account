6d857b28a349581d8bc306a926e4bbd261123bf3
unknown <nuty-girl@mail.ru>
2019-11-13 13:20:30 +0200

    Updates

---
+++ precache-manifest.181d8fc42c5e193df992a22342d2eaeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dde2620ec1c839da5813bad25971d87",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "65f4601a2f673e37b2a7",
+    "url": "/goit-react-hw-03-bank-account/static/css/2.da529cc7.chunk.css"
+  },
+  {
+    "revision": "47af85fd089d49773a93",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.6f8227b3.chunk.css"
+  },
+  {
+    "revision": "65f4601a2f673e37b2a7",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.be5a1c51.chunk.js"
+  },
+  {
+    "revision": "47af85fd089d49773a93",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.986d3f3b.chunk.js"
+  },
+  {
+    "revision": "88d4bb64cf1b1a6af667",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime-main.87af08b4.js"
+  }
+]);
